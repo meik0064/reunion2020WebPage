d1b70eece0c79af6e47a3842e0d5dd33b9fa6fd4
Meik <38284193+meik0064@users.noreply.github.com>
2019-05-24 17:35:29 +0200

    Edited Login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,6 +3,7 @@ import { JwtHelperService } from '@auth0/angular-jwt';
 import { HttpClient } from '@angular/common/http';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -10,8 +11,8 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  usernameVar : string
-  passwordVar : string
+  usernameVar: string
+  passwordVar: string
 
 
   loginFormParent = new FormGroup({
@@ -37,11 +38,30 @@ export class LoginComponent implements OnInit {
 
   onSubmit() {
     // TODO: Use EventEmitter with form value
+    console.log(this.loginFormParent.get("usernameForm").value, this.loginFormParent.get("passwordForm").value);
+    this.http.post('https://genforeningen-api.azurewebsites.net/admins/login', { "username": this.loginFormParent.get("usernameForm").value, "password": this.loginFormParent.get("passwordForm").value }, { withCredentials: true })
+      .subscribe((res) => {        
+        var token = res;
+        var isTokenExpired: boolean = this.jwtHelper.isTokenExpired(JSON.stringify(token));
+        console.log("isTokenExpired:" + isTokenExpired);
+        if (res.toString().length < 20) {
+
+          localStorage.setItem('key', 'ACCEPTED');
+          localStorage.setItem('token', JSON.stringify(res));
+        }
+      }, (err) => {
+        console.debug('In error func\n' + err);
+
+      });
+  }
+
+
+  logonAlt() {
+    // TODO: Use EventEmitter with form value
     console.log(this.loginFormParent.get("usernameForm").value, this.loginFormParent.get("passwordForm").value)
-    this.http.post('https://genforeningen-api.azurewebsites.net/admin/login', { "username": this.loginFormParent.get("usernameForm").value, "password": this.loginFormParent.get("passwordForm").value }, { withCredentials: true })
+    this.http.post('https://genforeningen-api.azurewebsites.net/admins/login', { "username": "test", "password": "password" }, { withCredentials: true })
       .subscribe((res) => {
         var token = res;
-        console.log(token);
         var isTokenExpired: boolean = this.jwtHelper.isTokenExpired(JSON.stringify(token));
         console.log("isTokenExpired:" + isTokenExpired);
         if (res.toString().length < 20) {
@@ -57,22 +77,32 @@ export class LoginComponent implements OnInit {
 
   logon() {
 
-    this.http.post('https://genforeningen-api.azurewebsites.net/admin/login', { "username": "test", "password": "password" }, { withCredentials: true })
-      .subscribe((res) => {
-        var token = res;
-        console.log(token);
-        var isTokenExpired: boolean = this.jwtHelper.isTokenExpired(JSON.stringify(token));
-        console.log("isTokenExpired:" + isTokenExpired);
-        if (res.toString().length < 20) {
-
-          localStorage.setItem('key', 'ACCEPTED');
-          localStorage.setItem('token', JSON.stringify(res));
-        }
-
-
-      });
-
+    this.http.post('https://genforeningen-api.azurewebsites.net/admins/login', { "username": "test", "password": "password" }, { withCredentials: true, observe: 'response' });
   }
 
+  /*.subscribe(response => {
+    console.log("Reached0", response);
+    var responseStatus = response.status;
+    console.log("Reached1", responseStatus);
+    console.log("Reached2", response.statusText);
+    //var isTokenExpired: boolean = this.jwtHelper.isTokenExpired(JSON.stringify(token));
+    //console.log("isTokenExpired:" + isTokenExpired);
+    if (responseStatus == 200) {
+      var token = response.body;
+      console.log(token);
+      console.log(this.jwtHelper.decodeToken(JSON.stringify(token)));
+      localStorage.setItem('key', 'ACCEPTED');
+      localStorage.setItem('token', JSON.stringify(token));
+    } else if (responseStatus == 403) {
+      console.log("Reached3", response.statusText);
+    } else if (responseStatus == 404) {
+      console.log("Reached4", response.statusText);
+    }
+
+
+  });*/
+
 }
+
+
 
